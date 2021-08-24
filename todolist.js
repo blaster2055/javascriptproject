aaec6eea89c9795bc73a17a60e3aedd8a6c087d6
blaster2055 <balster2055@gmail.com>
2021-08-24 15:30:50 +0200

    before change with Hatem

--- todolist.js
+++ todolist.js
@@ -1,19 +1,44 @@
-var myNodelist = document.getElementsByTagName("LI")
-var i  
-for (let i = 0; i <myNodelist .length; i++) {
-    const element = array[i];
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
+var user = [];
 
+function insertableau() {
+    if (document.getElementById('nom').value && document.getElementById('prenom').value && document.getElementById('age').value) {
+        user.push({
+            nom: document.getElementById('nom').value,
+            prenom: document.getElementById('prenom').value,
+            age: document.getElementById('age').value
+        })
+    }
+    affiche()
+    rest()
+}
 
+function affiche() {
+    document.getElementById("montableau").innerHTML = ""
+    user.forEach((value, i,) => {
+        document.getElementById("montableau").innerHTML += `<tr>
+        <td>${value.nom}</td>
+        <td>${value.prenom}</td>
+        <td>${value.age}</td>
+        <td>
+            <button onclick="supprimer(${i})">Supprimer</button>
+            <button onclick="achiche2()">Détails</button>
+        </td>`
 
+    })
+}
+function supprimer(i) {
 
+    user.splice(i, 1)
+    affiche()
+}
+function rest() {
+    document.querySelector('#nom').value = '';
+    document.querySelector('#prenom').value = '';
+    document.querySelector('#age').value = '';
 
+}
+function detaille(i) {
+    const found = user.find((element, index) => index == i)
+    console.log(found)
 
-    
-  }
-    
-  
+}
---
+++ trainingjs.js
@@ -0,0 +1,21 @@
+
+let num1=8
+let num2=2
+ document.getElementById('num1').textContent=num1
+ document.getElementById('num2').textContent=num2
+let resul=document.getElementById('"result"')
+
+function addition(){
+   result.textContent= num1+num2
+  
+}
+
+function soustraction(){
+    result.textContent=num1-num2
+}
+function multiplication(){
+   result.textContent= num1*num2
+}
+function devition(){
+   result.textContent= num1/num2
+}
