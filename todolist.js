e7a14e153f3343fb613fb77dfbd25d176e7ae9db
blaster2055 <balster2055@gmail.com>
2021-08-24 16:15:35 +0200

    ajout fonction update

--- todolist.js
+++ todolist.js
@@ -1,4 +1,5 @@
 var user = [];
+var index;
 
 function insertableau() {
     if (document.getElementById('nom').value && document.getElementById('prenom').value && document.getElementById('age').value) {
@@ -14,31 +15,46 @@ function insertableau() {
 
 function affiche() {
     document.getElementById("montableau").innerHTML = ""
-    user.forEach((value, i,) => {
+    user.forEach((value, i) => {
         document.getElementById("montableau").innerHTML += `<tr>
         <td>${value.nom}</td>
         <td>${value.prenom}</td>
         <td>${value.age}</td>
         <td>
             <button onclick="supprimer(${i})">Supprimer</button>
-            <button onclick="achiche2()">Détails</button>
+            <button onclick="detaille(${i})">Détails</button>
         </td>`
 
     })
 }
-function supprimer(i) {
 
+function supprimer(i) {
     user.splice(i, 1)
     affiche()
 }
+
 function rest() {
     document.querySelector('#nom').value = '';
     document.querySelector('#prenom').value = '';
     document.querySelector('#age').value = '';
-
 }
-function detaille(i) {
-    const found = user.find((element, index) => index == i)
-    console.log(found)
 
+function detaille(i){
+    index=i
+    const found = user[i]
+    document.getElementById('nom').value=found.nom
+    document.getElementById('prenom').value=found.prenom
+    document.getElementById('age').value=found.age
+}
+
+function update(){
+    const updatedUser = {
+        nom:document.getElementById('nom').value,
+        prenom:document.getElementById('prenom').value,
+        age:document.getElementById('age').value
+    }
+    
+    user.splice(index,1,updatedUser)
+    affiche()
+    rest()
 }
